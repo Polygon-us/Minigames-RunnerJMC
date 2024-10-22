0c8e60250474c94b3c84ab30e953b499d0d8b360
Juaner <juan.ramirez@polygon.us>
2024-10-22 16:59:04 -0500

    Update Rest

--- Assets/Scripts/Source/Rest/RestApiManager.cs
+++ Assets/Scripts/Source/Rest/RestApiManager.cs
@@ -40,29 +40,27 @@ public class RestApiManager : APIManager
             return;
         }
 
-        apiPaths = gameConfig.GetPathByType();
-
         base.Awake();
     }
 
     public void SetAuthToken(string token)
     {
-        transport.SetAuthToken(token);
+        Transport.SetAuthToken(token);
     }
 
     public void GetRequest<T>(string endpoint, Action<WebResult<T>> callback = null, string[] args = null)
     {
-        StartRequest(endpoint, path => transport.GET(PathWithArgs(path, args), callback));
+        StartRequest(endpoint, path => Transport.GET(PathWithArgs(path, args), callback));
     }
 
     public void PostRequest<T>(string endpoint, object data, Action<WebResult<T>> callback = null, string[] args = null)
     {
-        StartRequest(endpoint, path => transport.POST(PathWithArgs(path, args), JsonConvert.SerializeObject(data), callback));
+        StartRequest(endpoint, path => Transport.POST(PathWithArgs(path, args), JsonConvert.SerializeObject(data), callback));
     }
 
     public void PatchRequest<T>(string endpoint, object data, Action<WebResult<T>> callback)
     {
-        StartRequest(endpoint, path => transport.PATCH(path, JsonConvert.SerializeObject(data), callback));
+        StartRequest(endpoint, path => Transport.PATCH(path, JsonConvert.SerializeObject(data), callback));
     }
 
     public static string GetErrorResponse(string json)
@@ -72,7 +70,7 @@ public class RestApiManager : APIManager
 
     private void OnDestroy()
     {
-        transport?.SignOut();
+        Transport?.SignOut();
     }
 
     private static string PathWithArgs(string path, string[] args)
